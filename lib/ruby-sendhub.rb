90b583df24b1c1b444531263f8f4b31faeed7a6a
Alain Fontaine <criticerz@gmail.com>
2015-08-05 16:38:11 -0400

    testing new contacts method

--- lib/ruby-sendhub.rb
+++ lib/ruby-sendhub.rb
@@ -25,6 +25,8 @@ class SendHub
 			end
 		elsif meth.first == "get" && (meth.last == "threads" || meth.last == "messages" || !hsh[:id].nil?)
 			api_url = base_url + meth.last + "/" + hsh[:id].to_s + credentials
+		elsif meth.first == "get" && meth.last == "contacts")
+			api_url = base_url + meth.last + "/" + credentials + hsh.to_query
 		else
 			api_url = base_url + meth.last + credentials
 		end
